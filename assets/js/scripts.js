5c6a49beade7a178704b6aeb3a7573efb5a4e23c
yaptot <68444309+yaptot@users.noreply.github.com>
2020-09-06 16:51:09 +0800

    Updated styles

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -80,15 +80,43 @@ $(document).ready(function() {
                 type: 'POST',
                 data: form, 
                 success: function(result, s) {
-                    alert(result.msg);  
+                    alert(result.msg); 
+                    window.location.href ='/';     
                 },
                 error: function(xhr, s) {
-                    alert(xhr.responseJSON.msg);
+                    $(".warning-div").css("display", "inline-block");
+                    $("span#err-login").text(xhr.responseJSON.msg);
                 }
             })
          }
          else {
              $(".warning-div").css("display", "inline-block");  
          }
+    })
+
+    $("button#add-list-toggle").click(function() {
+        $("div#select-container").toggle();
+    })
+
+    $("button#add-review-toggle").click(function() {
+        $("form#review-form").toggle();
+    })
+
+    var login = document.getElementById("login-form");
+
+    login.addEventListener("keyup", function(event) {
+        if(event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("login-btn").click();
+        }
+    })
+
+    var register = document.getElementById("regForm");
+
+    register.addEventListener("keyup", function(event) {
+        if(event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("sign-up").click();
+        }
     })
 })
