49e962c9d62e4f77733e8003a90caf3d9ce02daf
terrence643 <terrence643@users.noreply.github.com>
2020-09-08 23:03:34 +0800

    index updated

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(session({
     cookie:{maxAge:1000*60*60*24}
 }))
 
+
 app.use(bodyparser.urlencoded({ extended: true}))
 app.use(bodyparser.json())
 app.use(express.static(__dirname + '/'));
