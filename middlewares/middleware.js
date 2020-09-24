29e271e9e2a1a62de46e620508888e5916ea3bdb
Eugene Perez <eugene_perez@dlsu.edu.ph>
2020-09-24 21:00:35 +0800

    added middleware for adding games

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -26,6 +26,19 @@ const functions = {
             }
         }
         res.status(401).send({msg: "Incorrect Credentials"})
+    },
+
+    validateGame    : async function(req, res, next){
+        var gameName = new RegExp('^' + req.body.gameName + '$', 'i');
+        let name = req.body.gameName
+        gameModel.aggregate([{$match: {name:gameName}}], function(err, data){
+            if(err) console.log(err)
+            else if(data.length > 0){
+                console.log(gameName)
+                console.log(data)
+               res.status(401).send({msg: name + " already exists"}); 
+            }else return next()
+        })
     }
 }
 
