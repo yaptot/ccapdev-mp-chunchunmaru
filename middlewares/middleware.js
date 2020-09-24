738b25fea781006ec21b894c3b5fd78f44ae510f
Eugene Perez <eugene_perez@dlsu.edu.ph>
2020-09-24 21:00:53 +0800

    Update middleware.js

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -34,8 +34,6 @@ const functions = {
         gameModel.aggregate([{$match: {name:gameName}}], function(err, data){
             if(err) console.log(err)
             else if(data.length > 0){
-                console.log(gameName)
-                console.log(data)
                res.status(401).send({msg: name + " already exists"}); 
             }else return next()
         })
